fbf859c4237100129abbfe58861a9e483684b98a
Cesar Huallpa <c.huallpa@alumni.ipade.mx>
2023-06-17 16:16:59 -0600

    seeds before testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+p "Creating user"
+User.create(password: "bbbags1234", email: "admin@b2g.com")
+User.create(password: "1bagforall", email: "c1@b2g.com")
+User.create(password: "2bagforall", email: "c2@b2g.com")
+User.create(password: "3bagforall", email: "c3@b2g.com")
+User.create(password: "4bagforall", email: "bo1@b2g.com")
+User.create(password: "5bagforall", email: "bo2@b2g.com")
+p "users created"

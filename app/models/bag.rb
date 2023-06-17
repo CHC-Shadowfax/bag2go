9f05f679e0e92644b6013d864d7380e4c6f4f01a
Anna Ibarra <annibrra@gmail.com>
2023-06-17 18:16:16 -0500

    update on bag validations and created validations for booking

--- app/models/bag.rb
+++ app/models/bag.rb
@@ -2,8 +2,7 @@ class Bag < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, length: { minimum: 5 }
-  validates :description, presence: true
-  validates :start_date, comparison: { greater_than: :end_date }
+  validates :description, presence: true, length: { minimum: 10 }
   validates :price_day, greater_than: 0
   validates :sku, presence: true, uniqueness: true, numericality: { only_integer: true }
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :bag
+
+  validates :start_date, :end_date, presence: true
+  validates :end_date, comparison: { greater_than: :start_date }
+  validates :start_date, comparison: { greater_than: Time.now }
+  validates :book_total_price, presence: true, numericality: { greater_than: 0 }
+  validates :real_end_date, comparison: { greater_than: Time.now }, allow_nil: true
+  validates :booking_desc, presence: true, length: { minimum: 10 }
 end

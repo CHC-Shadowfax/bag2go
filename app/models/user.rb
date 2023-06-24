5e35565754833eb5f4dbee7671c7bea99c7b842e
Gerardo Paz <paz2121@hotmail.com>
2023-06-24 16:18:38 -0600

    Homepage view

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ApplicationRecord
   has_many :bookings
 
   validates :user_type, presence: true, inclusion: { in: %w[owner renter] }
+
+
+  # def is_owner_of_this_(bag)
+  #   bag.user == self
+  # end
 end
+
+# # bag show page
+# if current_user.is_owner_of_this_(bag)
+# # show the bookings of the bag
+# end
